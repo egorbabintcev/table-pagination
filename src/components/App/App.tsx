1a5c20be349dc70da422c837e5b042d7c02b9980
Egor Babintsev <e.e.babintsev@yandex.ru>
2021-02-23 19:57:01 +0300

    Create table with loaded data. Add sort icon to svg sprite. Create placeholder for grid row

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.scss'
+import icons from './icons.svg'
 
 /* interface AppProps {} */
 
-const App: React.FC = (props) => <h1>Hello, world!</h1>
+interface IPerson {
+  id: string
+  name: string
+  age: number
+  balance: string
+}
+
+const App: React.FC = () => {
+  const [persons, setPersons] = useState<Array<IPerson>>([])
+
+  useEffect(() => {
+    // eslint-disable-next-line
+    ;(async () => {
+      try {
+        const response = await fetch('http://localhost:3001/persons')
+        const json = await response.json()
+        setPersons(json)
+      } catch (err) {
+        throw new Error(err)
+      }
+    })()
+  }, [])
+
+  return (
+    <div className="container">
+      <div className="table">
+        <div className="table-head">
+          <div className="table-head__cell">
+            <p>Name</p>
+            <button type="button">
+              <svg>
+                <use xlinkHref={`${icons}#sort`} />
+              </svg>
+            </button>
+          </div>
+          <div className="table-head__cell">
+            <p>Age</p>
+            <button type="button">
+              <svg>
+                <use xlinkHref={`${icons}#sort`} />
+              </svg>
+            </button>
+          </div>
+          <div className="table-head__cell">
+            <p>Balance</p>
+            <button type="button">
+              <svg>
+                <use xlinkHref={`${icons}#sort`} />
+              </svg>
+            </button>
+          </div>
+        </div>
+        {persons.map((p) => (
+          <div className="table-row" key={p.id}>
+            <div className="table-row__cell">
+              <p>{p.name}</p>
+            </div>
+            <div className="table-row__cell">
+              <p>{p.age}</p>
+            </div>
+            <div className="table-row__cell">
+              <p>{p.balance}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default App
